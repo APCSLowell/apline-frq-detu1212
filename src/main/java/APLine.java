3835d9193c407063a7158779f422cceefc81f1f8
detu1212 <143124200+detu1212@users.noreply.github.com>
2024-04-23 19:38:32 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,16 @@
 public class APLine
 {
-  /* your code here */
+  private double a1, b1, c1;
+  public APLine(int a, int b, int c){
+    this.a1 = a;
+    this.b1 = b;
+    this.c1 = c;
+  }
+  public double getShape(){
+    return - (this.a1/this.b1);
+  }
+  public boolean isOnLine (int x, int y){
+    return (0==(this.a * x) + (this.b * y) + this.c);
+  }
   
 }
