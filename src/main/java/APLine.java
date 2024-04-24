ffa12edf39eade7b06bcd085f5a15086ec830c8c
detu1212 <143124200+detu1212@users.noreply.github.com>
2024-04-23 19:49:39 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,16 +1,15 @@
 public class APLine
 {
-  private double a1, b1, c1;
-  public APLine(int a, int b, int c){
-    this.a1 = a;
-    this.b1 = b;
-    this.c1 = c;
-  }
-  public double getShape(){
-    return - (this.a1/this.b1);
-  }
-  public boolean isOnLine (int x, int y){
-    return (0==(this.a * x) + (this.b * y) + this.c);
-  }
-  
+ private double a1, b1, c1;
+ public APLine(int a, int b, int c) {
+ this.a1 = a;
+ this.b1 = b;
+ this.c1 = c;
+ }
+ public double getSlope() {
+ return -(this.a1 / this.b1);
+ }
+ public boolean isOnLine(int x, int y) {
+ return (0 == (this.a1 * x) + (this.b1 * y) + this.c1);
+ }
 }
